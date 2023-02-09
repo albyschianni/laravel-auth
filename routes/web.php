c87cdb71b363a30a5ebe7dafddc293f548dee46c
albyschianni <alberto.schianni@gmail.com>
2023-02-09 23:16:16 +0100

    dashboard redirect to home & edit/create/delete visibile under login

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,33 @@ Route::get('/', [MainController::class, 'home'])
 Route::get('/project/show/{project}', [MainController::class, 'projectShow'])
     -> name('project.show');
 
-Route::get('/logged/project/delete/{project}', [MainController::class, 'projectDelete']) -> middleware(['auth','verified'])
-    -> name('project.delete');
 
-Route::get('/logged/project/create', [MainController::class, 'projectCreate']) -> middleware(['auth','verified'])
-    -> name('project.create');
 
 Route :: post('/project/store', [MainController :: class, 'projectStore'])
     -> name('project.store');
-
-Route::get('/logged/project/edit/{project}', [MainController::class, 'projectEdit']) -> middleware(['auth','verified'])
-    -> name('project.edit');
 
 Route :: post('/project/update/{project}', [MainController :: class, 'projectUpdate'])
     -> name('project.update');
 
 
-    
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect() -> route('home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
+
 Route::middleware('auth')->group(function () {
+    
+    Route::get('/logged/project/delete/{project}', [MainController::class, 'projectDelete']) 
+    -> name('project.delete');
+    Route::get('/logged/project/edit/{project}', [MainController::class, 'projectEdit']) 
+    -> name('project.edit');
+    Route::get('/logged/project/create', [MainController::class, 'projectCreate']) 
+    -> name('project.create');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
